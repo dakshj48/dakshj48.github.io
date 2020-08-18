60489274de7c66f8f3afa30d690142c870a92bb4
Daksh <dakshjain48@gmail.com>
2020-08-18 04:28:49 -0700

    Added some components for the portolio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Education from './components/Education';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Daksh Jain
+      </h1>
+      <Education />
+      <Experience />
+      <Projects />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Education.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Education = () => {
+  return (
+    <div>
+      <h1>
+        Education
+      </h1>
+      <table>
+        <tbody>
+          <tr>
+            <td width="750px">
+              <h2>
+                Bachelor of Science at the University of Massachusetts, Amherst
+              </h2>
+            </td>
+            <td>
+              Sep. 2016 - May 2020
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h3>Double major in Computer Science and Mathematics</h3>
+            </td>
+            <td>
+              GPA: 3.8/ 4.0
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <h4>
+        Coursework: Web Development, Machine Learning, Natural Language Processing, Networks, Formal Language Theory, 
+        Game Programming, Algorithms, Operating Systems
+      </h4>
+    </div>
+  )
+}
+
+export default Education
