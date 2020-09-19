9df5e0cf35ba03bd42bc154df17018d5c032e5a6
Daksh <dakshjain48@gmail.com>
2020-09-19 05:14:01 -0700

    Updates

---
+++ precache-manifest.ce6c7b66d17de1611f5f83d9d980fe1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6679d67aa4262346727266e8e11279",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49492b3fa53810baae3b",
+    "url": "/static/css/main.ab6e84e9.chunk.css"
+  },
+  {
+    "revision": "e18c8c2ec885c1f76338",
+    "url": "/static/js/2.49c608fc.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.49c608fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49492b3fa53810baae3b",
+    "url": "/static/js/main.b7e78d24.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "31afdfc5311374d35191319d2fac5983",
+    "url": "/static/media/background.31afdfc5.jpg"
+  },
+  {
+    "revision": "710dc40c7c2d1f4aacfcb46e9a973288",
+    "url": "/static/media/profile_photo.710dc40c.jpeg"
+  }
+]);
