f0519390184180b8a6af2e840dd811ca3048c07b
unknown <dakshjain48@gmail.com>
2021-06-07 00:58:58 +0530

    Fix: warnings

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import Education from './Education';
 import Experience from './Experience';
 import Projects from './Projects';
