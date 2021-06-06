1208b8a7deff7c7ff2a31f130c7628bf29457341
unknown <dakshjain48@gmail.com>
2021-06-07 03:22:50 +0530

    Updates

--- precache-manifest.915b317a0e06aab68599e1025b76c27c.js
+++ precache-manifest.915b317a0e06aab68599e1025b76c27c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "adfd27059e80f572c43b21dfd818d05d",
+    "revision": "66e2bfaa4adad976d62052b4e5e02279",
     "url": "/index.html"
   },
   {
-    "revision": "e94ea3dbea9e3324c9b3",
-    "url": "/static/css/main.77d9b603.chunk.css"
+    "revision": "bec6b8f90f98e639e0a3",
+    "url": "/static/css/main.42b8939b.chunk.css"
   },
   {
     "revision": "e48f9180bb8de4ba2cc2",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.6010731c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e94ea3dbea9e3324c9b3",
-    "url": "/static/js/main.4abf3dd6.chunk.js"
+    "revision": "bec6b8f90f98e639e0a3",
+    "url": "/static/js/main.6009fafc.chunk.js"
   },
   {
     "revision": "f139b624154a37532bb8",
